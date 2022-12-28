3a396ec02da33b5a1499570529fe252d671c830e
[Narmin Bagirova] <[narminnbagirova@gmail.com]>
2022-12-28 19:05:12 +0400

    1

---
+++ algorithm task 1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace algorithm_task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number=int.Parse(Console.ReadLine());
+            int countDigit = (int)Math.Log10(number) + 1;
+       
+            Console.WriteLine(countDigit);
+        }
+
+    }
+}
