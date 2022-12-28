8cecb50a3aa3be98c73a98311a88679a529dfeca
[Narmin Bagirova] <[narminnbagirova@gmail.com]>
2022-12-28 19:05:12 +0400

    Add project files.

---
+++ algorithm task 1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace algorithm_task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ededin cut yerde duran reqemlerinin kvadrati:");
+            int number = 123046;
+            int sum = 0, a;
+            ;
+            while (number != 0)
+            {
+
+                a = number % 10;
+                number = number / 100;
+                sum += Convert.ToInt32(Math.Pow(a, 2));
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
