78c8376a0a18d6a075d233f118a5ed72de79dd0c
[Narmin Bagirova] <[narminnbagirova@gmail.com]>
2022-12-29 16:46:42 +0400

    change

--- algorithm task 1/Program.cs
+++ algorithm task 1/Program.cs
@@ -6,8 +6,24 @@
         {
             int number = int.Parse(Console.ReadLine());
             int countDigit = (int)Math.Log10(number) + 1;
+            int digit, i = 0;
+            if (countDigit % 2 == 0)
+                i = 0;
+            else
+                i = i;
+            while (number>0)
+            {
+                digit = number % 10;
+                number /= 10;
 
-            Console.WriteLine(countDigit);
+                if (i%2==0)
+                {
+                    Console.WriteLine(digit);
+                }
+                i++;
+            }
+
+           
         }
 
     }
